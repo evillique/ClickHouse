cfaee6f9c818fb29ec9733ff2b4ead315911500b
Petr Vasilev <vahpetr@gmail.com>
2023-09-07 13:59:32 +0300

    Fix nats high cpu usage

--- src/Storages/NATS/StorageNATS.cpp
+++ src/Storages/NATS/StorageNATS.cpp
@@ -109,6 +109,7 @@ StorageNATS::StorageNATS(
             }
 
             LOG_DEBUG(log, "Connect attempt #{} failed, error: {}. Reconnecting...", i + 1, nats_GetLastError(nullptr));
+            std::this_thread::sleep_for(std::chrono::milliseconds(configuration.reconnect_wait));
         }
     }
     catch (...)
@@ -591,8 +592,9 @@ void StorageNATS::streamingToViewsFunc()
                 if (streamToViews())
                 {
                     /// Reschedule with backoff.
-                    do_reschedule = false;
                     break;
+                } else {
+                    do_reschedule = false;
                 }
 
                 auto end_time = std::chrono::steady_clock::now();
